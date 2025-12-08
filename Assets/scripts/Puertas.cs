f97a3b5461abb8013f56f37896a30282c664eef5
Tavotav <137920092+Tavotav@users.noreply.github.com>
2025-12-07 23:31:16 -0300

    Avance en puertas y fogatas

--- Assets/scripts/Puertas.cs
+++ Assets/scripts/Puertas.cs
@@ -1,8 +1,9 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 public class Puertas : MonoBehaviour
 {
-    private bool enColisionPuerta = false;
+    
     void Start()
     {
         
@@ -11,27 +12,70 @@ public class Puertas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (enColisionPuerta && Input.GetKeyDown(KeyCode.Space))
-        {
-
-        }
+       
     }
     private void OnCollisionEnter2D(Collision2D coll)
     {
-        if (coll.gameObject.CompareTag("hogera"))
+        if (coll.gameObject.CompareTag("Puerta inicial"))
         {
-
-            enColisionPuerta = true;
+            SceneManager.LoadScene("Sala 2");
+        } else if (coll.gameObject.CompareTag("Puerta spawn"))
+        {
+            SceneManager.LoadScene("Sala spawn");
         }
+        else if (coll.gameObject.CompareTag("puerta 3"))
+        {
+            SceneManager.LoadScene("Sala 3 vertical 1");
+        }
+        else if (coll.gameObject.CompareTag("puerta 2"))
+        {
+            SceneManager.LoadScene("Sala 2");
+        }
+        else if (coll.gameObject.CompareTag("puerta 4"))
+        {
+            SceneManager.LoadScene("Sala 4");
+        }
+        else if (coll.gameObject.CompareTag("puerta 3A"))
+        {
+            SceneManager.LoadScene("Sala 3 vertical 1");
+
+        }
+        else if (coll.gameObject.CompareTag("puerta 5"))
+        {
+            SceneManager.LoadScene("Sala 5 vertical");
+        }
+        else if (coll.gameObject.CompareTag("puerta 4F"))
+        {
+            SceneManager.LoadScene("Sala 4");
+        }
+        else if (coll.gameObject.CompareTag("Spawn 2"))
+        {
+            SceneManager.LoadScene("Sala Spawn 1");
+        }
+        
+        else if (coll.gameObject.CompareTag("puerta 6"))
+        {
+            SceneManager.LoadScene("Sala 6");
+        }
+        else if (coll.gameObject.CompareTag("puerta 5A"))
+        {
+            SceneManager.LoadScene("Sala 5 vertical");
+        }
+        else if (coll.gameObject.CompareTag("Sala Jefe"))
+        {
+            SceneManager.LoadScene("Sala Jefe");
+        }
+        else if (coll.gameObject.CompareTag("Vuelta Spawn 2"))
+        {
+            SceneManager.LoadScene("Sala Spawn 1");
+        }
+        else if (coll.gameObject.CompareTag("Puerta 6D"))
+        {
+            SceneManager.LoadScene("Sala 6");
+        }
+
+
     }
 
-    private void OnCollisionExit2D(Collision2D coll)
-    {
-
-        if (coll.gameObject.CompareTag("hogera"))
-        {
-
-            enColisionPuerta = false;
-        }
-    }
+   
 }
--- Assets/scripts/fogata.cs
+++ Assets/scripts/fogata.cs
@@ -7,69 +7,32 @@ public class fogata : MonoBehaviour
     private bool enColisionConHogera1 = false;
     private bool enColisionConHogera2 = false;
     private bool DesbloqueadoConHogera2 = false;
-    private GameObject objetoDestino;
+    
 
     void Update()
     {
-        if (enColisionConHogera1 && Input.GetKeyDown(KeyCode.K))
+        if (enColisionConHogera1 && Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.sceneLoaded += Spawn2;
-
-
-            
-           if (DesbloqueadoConHogera2 == true)
-             {
-                SceneManager.LoadScene("Sala Spawn 1");
-            }
-
-        }
-        
-       
-        else if (enColisionConHogera2 && Input.GetKeyDown(KeyCode.K))
-        {
-            SceneManager.sceneLoaded += Spawn1;
-
-
 
             if (DesbloqueadoConHogera2 == true)
-            {
-                SceneManager.LoadScene("Sala Spawn");
+             {
+                 SceneManager.LoadScene("Sala Spawn 1");
             }
 
-
+        }
+        
+       
+        else if (enColisionConHogera2 && Input.GetKeyDown(KeyCode.Space))
+        {
+            
+                SceneManager.LoadScene("Sala Spawn");
+            
         }
        
 
 
     }
-    private void Spawn2(Scene scene, LoadSceneMode mode)
-    {
-        
-        GameObject objetoDestino = GameObject.FindGameObjectWithTag("hogera 2");
-
-        if (objetoDestino != null)
-        {
-            transform.position = objetoDestino.transform.position;
-        }
-       
-
-        
-        SceneManager.sceneLoaded -= Spawn2;
-    }
-    private void Spawn1(Scene scene, LoadSceneMode mode)
-    {
-
-        GameObject objetoDestino = GameObject.FindGameObjectWithTag("hogera");
-
-        if (objetoDestino != null)
-        {
-            transform.position = objetoDestino.transform.position;
-        }
-
-
-
-        SceneManager.sceneLoaded -= Spawn1;
-    }
+    
 
     private void OnTriggerEnter2D(Collider2D coll)
     {
